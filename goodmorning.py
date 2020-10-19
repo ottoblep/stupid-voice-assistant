e3de8da0e4fcdceaff3ca16e290b42ce00e53c2b
ottoblep <57066925+ottoblep@users.noreply.github.com>
2020-10-20 00:00:54 +0200

    Added STT using Vosk and made interaction possible

--- goodmorning.py
+++ goodmorning.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import sys
 from datetime import datetime
 from datetime import date
 import os
@@ -13,25 +14,15 @@ import pycurl
 import pprint
 from io import BytesIO
 import io
-
-#Forbes Quote
-quote_url = "http://www.forbes.com/forbesapi/thought/uri.json?enrich=true&query=1&relatedlimit=5"
+from vosk import Model, KaldiRecognizer
+import pyaudio
+from playsound import playsound
+import json
+import threading
 
 #Fixed Texts
-greetings = ["Guten Morgen","Wach auf","Mach dich auf die Socken","Steh Auf","Zeit Aufzustehen"]
-quoteintros = ["dein zitat lautet","hier ist dein zitat","ein zitat für dich","hier ist was zum nachdenken","hör dir das an","ein weiser mann sagte mal"]
-
-##def activator():
-##    while True:
-##        time = str(datetime.now().time())
-##        timelist = time.split(":")
-##        # Generiere Sprache um 5:00
-##        if timelist[0]=5 and timelist[1]=0:
-##            createmessage()
-##            computemessage()
-##        # Warte auf extra Signal zum Abspielen von 5-10
-##        if timelist[0]<10 and timelist[0]>5 and ADDITIONAL CONDITION:
-##          ABSPIELEN
+greetings = ["Guten Morgen","Zeit Aufzustehen"]
+quoteintros = ["hier ist ein zitat","ein zitat für dich","hier ein zitat","das zitat des tages","das heutige zitat"]
 
 def monatsname(monat):
     if monat==1: return "Januar"
@@ -46,6 +37,7 @@ def monatsname(monat):
     if monat==10: return "Oktober"
     if monat==11: return "November"
     if monat==12: return "Dezember"
+    
 def wochentag(tag):
     if tag=="Monday": return "Montag"
     if tag=="Tuesday": return "Dienstag"
@@ -54,7 +46,8 @@ def wochentag(tag):
     if tag=="Friday": return "Freitag"
     if tag=="Saturday": return "Samstag"
     if tag=="Sunday": return "Sonntag"
-def getweather():   
+    
+def getweather():
     opener = urllib.request.FancyURLopener({})
     f = opener.open("https://www.wetter.com/wetter_aktuell/wettervorhersage/3_tagesvorhersage/deutschland/gilching/DE0003429.html")
     content = f.read()
@@ -67,14 +60,9 @@ def getweather():
     weathertext = content[start+66:end-24]
     weathertext=weathertext.replace("°C","°")
     if start!=-1 and end!=-1:
-        return weathertext 
-
+        return weathertext
+    
 def getquote():
-##    response = requests.get(quote_url)
-##    data = response.json()
-##    quote=data['thought']['quote'].strip("(' )")
-##    return quote
-
     opener = urllib.request.FancyURLopener({})
     f = opener.open("https://www.zitatdestages.net/")
     content = f.read()
@@ -83,15 +71,11 @@ def getquote():
     end = content.find("</p><p class=\"blog")
     quotetext = content[start+22:end]
     if start!=-1 and end!=-1:
-        return quotetext 
-
-def computemessage():
-    #creates wav from sentences.txt
-    os.system("gtts-cli -l de -f message.txt -o out.mp3 ")
-    os.system("vlc out.mp3")
+        out = random.choice(quoteintros)+". "+quotetext
+        return out
+    else: print("Scraper Failed")
     
-    
-def createmessage():
+def getdate():
     #Date
     my_date = date.today()
     datelist = str(my_date).split("-")
@@ -99,26 +83,62 @@ def createmessage():
     month = datelist[1]
     day = datelist[2]
     weekday = calendar.day_name[my_date.weekday()]
-    #Time
-    time = str(datetime.now().time())
-    timelist = time.split(":")
-    hour = timelist[0];
-    minute = timelist[1];
-    second = timelist[2];
-    #Weather
-    weathermessage=getweather()
-    #Compose Message
     message=random.choice(greetings)+". Es ist "+wochentag(weekday)+". "+day+"ter "+monatsname(int(month))+" "+year+"."
-    message+=" "+weathermessage+" "
-    message+=random.choice(quoteintros)+". "+getquote()       
-    print(message)
+    return message
+def getweather():
+    opener = urllib.request.FancyURLopener({})
+    f = opener.open("https://www.wetter.com/wetter_aktuell/wettervorhersage/3_tagesvorhersage/deutschland/gilching/DE0003429.html")
+    content = f.read()
+    content=str(content, encoding='utf-8', errors='strict')
+    content=content.replace("<br />","")
+    content=content.replace("<br>","")
+    content=content.replace("<p>","")
+    start = content.find("<h3>Wetter heute,")
+    end = content.find("Böen")
+    weathertext = content[start+66:end-3]
+    weathertext=weathertext.replace("°C","°")
+    if start!=-1 and end!=-1:
+        return weathertext 
+    
+def play(message):
+    print("Output: "+message)
     # process Unicode text
     with io.open("message.txt",'w',encoding='utf8') as f:
         f.write(message)
+    #creates wav from sentences.txt
+    os.system("gtts-cli -l de -f message.txt -o out.mp3 ") 
+    os.system("vlc out.mp3")
+    
+def actionthread(command):
+    print ("Creating Thread for "+command)
+    if command=="wetter": play(getweather())
+    if command=="datum": play(getdate())
+    if command=="zitat": play(getquote())
+    if command=="test": play("Test Test Test")
+    
+def listenloop():
+    if not os.path.exists("model"):
+        print ("Please download the model from https://alphacephei.com/vosk/models and unpack as 'model' in the current folder.")
+        exit (1)
+    model = Model("model")
+    rec = KaldiRecognizer(model, 16000)
+    p = pyaudio.PyAudio()
+    stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8000)
+    stream.start_stream()
+    while True:
+        data = stream.read(4000)
+        if len(data) == 0:
+            break
+        if rec.AcceptWaveform(data):
+            res = json.loads(rec.Result())
+ #           actionthread(res['text'])
+            
+## For Threading call ActionThread like this but there are file permission problems
+            t = threading.Thread(target=actionthread, args=(res['text'],))
+            t.start()
 
-
-createmessage()
-computemessage()
+       
+listenloop()
 
 
 
